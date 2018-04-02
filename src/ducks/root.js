715ffc8e7fcdaa791a4ffbd20667f492ffc09008
yazeedb <ybzadough@gmail.com>
2018-04-01 20:04:45 -0400

    reset button?

--- src/ducks/root.js
+++ src/ducks/root.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
-import { reducer as signupReducer } from '/features/Signup/Signup.duck';
 
 export default combineReducers({
-	signupReducer
+	sample: () => null
 });
--- src/store.js
+++ src/store.js
@@ -1,9 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from '/ducks/root';
-import rootEpic from '/epics/root';
 
 export default createStore(
 	rootReducer,
-	applyMiddleware(rootEpic, logger)
+	applyMiddleware(logger)
 );
